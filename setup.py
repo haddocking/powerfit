d5d63df4949cf64e750ffc08412fd9ab4425a262
Gydo <g.c.p.vanzundert@uu.nl>
2015-02-01 14:59:44 +0100

    Update

--- setup.py
+++ setup.py
@@ -5,29 +5,36 @@ from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 import numpy
 
-ext_modules = [Extension("powerfit.libpowerfit",
-                         [os.path.join("src", "libpowerfit.pyx")],
-                         include_dirs=[numpy.get_include()],
-                         )
-              ]
+def main():
+    packages = ['powerfit', 'powerfit.IO']
 
-scripts = [os.path.join('scripts', 'powerfit')]
+    ext_modules = [Extension("powerfit.libpowerfit",
+                             [os.path.join("src", "libpowerfit.pyx")],
+                             include_dirs=[numpy.get_include()],
+                             )
+                  ]
 
-package_data = {'powerfit': [os.path.join('data', '*.npy'), 
-                             os.path.join('kernels', '*.cl'), 
-                             os.path.join('IO', '*.py')],
-               }
+    package_data = {'powerfit': [os.path.join('data', '*.npy'), 
+                                 os.path.join('kernels', '*.cl'), 
+                                 ]
+                   }
 
-setup(name="powerfit",
-      version='0.1.0',
-      description='PDB fitting in cryoEM maps',
-      author='Gydo C.P. van Zundert',
-      author_email='g.c.p.vanzundert@uu.nl',
-      packages=['powerfit'],
-      cmdclass = {'build_ext': build_ext},
-      ext_modules = cythonize(ext_modules),
-      package_data = package_data,
-      scripts=scripts,
-      requires=['numpy', 'scipy', 'cython'],
-      include_dirs=[numpy.get_include()],
-    )
+    scripts = [os.path.join('scripts', 'powerfit')]
+
+    setup(name="powerfit",
+          version='0.1.0',
+          description='PDB fitting in cryoEM maps',
+          author='Gydo C.P. van Zundert',
+          author_email='g.c.p.vanzundert@uu.nl',
+          packages=packages,
+          cmdclass = {'build_ext': build_ext},
+          ext_modules = cythonize(ext_modules),
+          package_data = package_data,
+          scripts=scripts,
+          requires=['numpy', 'scipy', 'cython'],
+          include_dirs=[numpy.get_include()],
+        )
+
+if __name__=='__main__':
+
+    main()
