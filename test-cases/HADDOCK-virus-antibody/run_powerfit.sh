b70cd352c3712fbab69aa8f0b0560f7a93037b4f
Gydo <g.c.p.vanzundert@uu.nl>
2015-03-31 17:24:17 +0200

    Typo fix

--- test-cases/HADDOCK-virus-antibody/run_powerfit.sh
+++ test-cases/HADDOCK-virus-antibody/run_powerfit.sh
@@ -1,2 +1,2 @@
-powerfit 3c5x_seigdA_renum.pdb subunit.xplor 21 -a 5.27 -l -nr -d results/3c5x
+powerfit 3c5x_segidA subunit.xplor 21 -a 5.27 -l -nr -d results/3c5x
 powerfit Fab_segidB.pdb subunit.xplor 21 -a 5.27 -l -nr -d results/fab
