a82b00180e93b7d6e62b4b92727d445fc109498a
FloLangenfeld <florent.langenfeld@gmail.com>
2018-06-22 10:27:19 +0200

    Updated print() statement in powerfit.py

--- powerfit/powerfit.py
+++ powerfit/powerfit.py
@@ -105,7 +105,7 @@ def get_filetype_template(fname):
 def write(line):
     """Write line to stdout and logfile."""
     if stdout.isatty():
-        print line
+        print(line)
     logging.info(line)
 
 
