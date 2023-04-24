6bc720d7ce9191845d71122de988b44ebe6fb828
Luc <hllelli2@liverpool.ac.uk>
2023-04-24 19:36:49 +0100

    small bug fix 	modified:   powerfit/__init__.py

--- powerfit/__init__.py
+++ powerfit/__init__.py
@@ -1,4 +1,4 @@
-from .volume import Volume, structure_to_shape_like, structure_to_shape_TEMPy
+from .volume import Volume, structure_to_shape_like
 from .structure import Structure
 from .rotations import proportional_orientations, quat_to_rotmat
 from .helpers import determine_core_indices
