917a830de909dae82e8940114df79cf230e22968
Luc <hllelli2@liverpool.ac.uk>
2023-04-24 22:46:51 +0100

    type checker bug with xyz_fixed 	modified:   powerfit/powerfit.py

--- powerfit/powerfit.py
+++ powerfit/powerfit.py
@@ -310,7 +310,7 @@ def main(
 
     write("Template file read from: {:s}".format(structure.filename))
     if xyz_fixed:
-        if isinstance(xyz_fixed, Path):
+        if isinstance(xyz_fixed, (Path,str)):
             xyz_fixed_structure = Structure.fromfile(str(xyz_fixed.resolve()))
         write("Fixed model file read from: {:s}".format(xyz_fixed_structure.filename))
 
