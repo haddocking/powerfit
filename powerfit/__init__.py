603955158f010f0e221d6ecfde434fb32eda5ed7
Luc <hllelli2@liverpool.ac.uk>
2023-07-20 11:38:53 +0100

    refactored code

--- powerfit/__init__.py
+++ powerfit/__init__.py
@@ -1,5 +1,5 @@
-from .volume import Volume, structure_to_shape_like
-from .structure import Structure
+from .volume_helpers import structure_to_shape_like
+from .structure_helpers import Structure
 from .rotations import proportional_orientations, quat_to_rotmat
 from .helpers import determine_core_indices
 from .powerfit import main
