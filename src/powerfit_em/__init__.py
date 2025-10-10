b3028ec1705f4f91c4b1c4fb73295b1b6285ea86
Bart Schilperoort <b.schilperoort@esciencecenter.nl>
2025-10-10 14:13:53 +0200

    add dunder all (PEP8)

--- src/powerfit_em/__init__.py
+++ src/powerfit_em/__init__.py
@@ -4,3 +4,13 @@ from .volume import Volume, structure_to_shape_like
 from .structure import Structure
 from .rotations import proportional_orientations, quat_to_rotmat
 from .helpers import determine_core_indices
+
+
+__all__ = [
+    "determine_core_indices",
+    "proportional_orientations",
+    "quat_to_rotmat",
+    "Structure",
+    "structure_to_shape_like",
+    "Volume",
+]
